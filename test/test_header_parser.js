2f6687d01dfa85d705aeff272a895dff5a282fb9
larabr <7375870+larabr@users.noreply.github.com>
2026-08-25 17:57:15 +0200

    Tests: update ISO-2022-JP test expectation following WebKit changing support
    See https://bugs.webkit.org/show_bug.cgi?id=312106 .
    
    Related: https://github.com/ProtonMail/jsmimeparser/issues/1 .

--- test/test_header_parser.js
+++ test/test_header_parser.js
@@ -1,7 +1,7 @@
 
 import { assert } from "vitest";
 import { headerparser } from "../lib/jsmime";
-import { isFirefox } from "./utils";
+import { isChromium } from "./utils";
 
 function arrayTest(data, fn) {
   fn.toString = function() {
@@ -1132,7 +1132,7 @@ describe("headerparser", function() {
       // GyRCJCIbKEI= is the base64 encoding of ESC$B$"ESC(B.
       [
         "=?ISO-2022-JP?B?GyRCJCIbKEI=?==?ISO-2022-JP?B?GyRCJCIbKEI=?=",
-        isFirefox() ? "ああ" : "あ\ufffdあ" // see https://github.com/ProtonMail/jsmimeparser/issues/1
+        isChromium() ? "あ\ufffdあ" : "ああ" // see https://github.com/ProtonMail/jsmimeparser/issues/1
       ],
 
       // Tolerate invalid split of character, € = 0xE2 0x82 0xAC in UTF-8.
